a9416c258d7f90d34eee7f1cbcd06e31f56fc1d9
KazuProg <91452566+KazuProg@users.noreply.github.com>
2026-07-16 14:15:18 +0900

    feat: import hdr script to ~/.local/bin/my

--- dot_config/sh/env.sh
+++ dot_config/sh/env.sh
@@ -3,7 +3,7 @@ export XDG_CONFIG_HOME="${HOME}"/.config
 export XDG_CACHE_HOME="${HOME}"/.cache
 export XDG_DATA_HOME="${HOME}"/.local/share
 export XDG_STATE_HOME="${HOME}"/.local/state
-export PATH="${HOME}"/.local/bin:"${PATH}"
+export PATH="${HOME}"/.local/bin:"${HOME}"/.local/bin/my:"${PATH}"
 
 # Rust
 export RUSTUP_HOME="${XDG_DATA_HOME}"/rustup
